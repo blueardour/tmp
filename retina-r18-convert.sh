cb5383de0fc6090fe3ad9d168b9de0554f44f261
liujing@hn <liujing@hn>
2020-11-07 10:22:36 +0800

    RetinaNet R18

---
+++ retina-r18-convert.sh
@@ -0,0 +1,11 @@
+
+cd ../detectron2
+
+bit=2
+
+python ../ldn-quantization/tools.py --keyword update,raw --old pretrained/retinanet/retinanet_R_18_1x_Full_BN_dorefa_clip_${bit}bit_no_lp_pretrained/model_final.pth --new pretrained/retinanet/retinanet_R_18_1x_Full_BN_dorefa_clip_${bit}bit_no_lp_pretrained/error-fpn.pth --mf ../tmp/retina-r18-from_jing.txt-sort --mt ../tmp/retina-r18-to.txt-sort 
+
+python ../ldn-quantization/tools.py --keyword reshape --mf weights/det-resnet18/reshape.txt --old pretrained/retinanet/retinanet_R_18_1x_Full_BN_dorefa_clip_${bit}bit_no_lp_pretrained/error-fpn.pth --new pretrained/retinanet/retinanet_R_18_1x_Full_BN_dorefa_clip_${bit}bit_no_lp_pretrained/error-reshape.pth
+
+cd - 
+
