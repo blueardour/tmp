5923aa4a9b7bf20b25d276dcb61d5158cf910e81
blueardour@server <blueardour@server>
2020-11-09 10:11:13 +1030

    Fix bugs

--- fcos-r34/convert.sh
+++ fcos-r34/convert.sh
@@ -1,11 +1,9 @@
 
-cd ../../detectron2
+cd ../../AdelaiDet/
 
 bit=2
 
-python ../ldn-quantization/tools.py --keyword update,raw --old pretrained/fcos/R_34_1x-Full_SyncBN_dorefa_clip_${bit}bit_no_lp_pretrained/model_final.pth --new pretrained/fcos/R_34_1x-Full_SyncBN_dorefa_clip_${bit}bit_no_lp_pretrained/import_jing.pth --mf weights/det-resnet34/lsq-2bit-from_liu.txt-sort --mt weights/det-resnet34/lsq-2bit-to_FixFPN.txt-sort 
-
-#lsq-2bit-from_liu.txt-sort  lsq-2bit-to_FixFPN.txt-sort
+python ../ldn-quantization/tools.py --keyword update,raw --old pretrained/fcos/R_34_1x-Full_SyncBN_dorefa_clip_${bit}bit_no_lp_pretrained/model_final.pth --new pretrained/fcos/R_34_1x-Full_SyncBN_dorefa_clip_${bit}bit_no_lp_pretrained/import_jing.pth --mf ../tmp/fcos-r34/lsq-2bit-from_liu.txt-sort --mt ../tmp/fcos-r34/lsq-2bit-to_FixFPN.txt-sort 
 
 python ../ldn-quantization/tools.py --keyword reshape --old pretrained/fcos/R_34_1x-Full_SyncBN_dorefa_clip_${bit}bit_no_lp_pretrained/import_jing.pth --mf weights/det-resnet18/reshape.txt --new pretrained/fcos/R_34_1x-Full_SyncBN_dorefa_clip_${bit}bit_no_lp_pretrained/reshape.pth
 
--- r50-convert.sh
+++ r50-convert.sh
@@ -1,7 +1,7 @@
 
 cd ../AdelaiDet/
 
-bit=2
+bit=4
 
 python ../ldn-quantization/tools.py --keyword update,raw --old pretrained/fcos/R_50_1x-FPN_SyncBN-FCOS-SyncBN_no_lp_pretrained_${bit}bit/model_final.pth --new pretrained/fcos/R_50_1x-FPN_SyncBN-FCOS-SyncBN_no_lp_pretrained_${bit}bit/import_jing.pth --mf ../tmp/fcos-r50-from.txt-sort --mt ../tmp/fcos-r50-to.txt-sort
 
