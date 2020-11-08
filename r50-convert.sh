3d89f2aa75015932d87e36bc42373cb426c826b7
blueardour@server <blueardour@server>
2020-11-08 12:30:03 +1030

    fcos r50

--- r50-convert.sh
+++ r50-convert.sh
@@ -3,7 +3,8 @@ cd ../AdelaiDet/
 
 bit=2
 
-python ../ldn-quantization/tools.py --keyword update,raw --old pretrained/fcos/R_50_1x-FPN_SyncBN-FCOS-SyncBN_no_lp_pretrained_${bit}bit/model_final.pth --new pretrained/fcos/R_50_1x-FPN_SyncBN-FCOS-SyncBN_no_lp_pretrained_${bit}bit/import_jing.pth --mf ../tmp/lsq-2bit_to_reshape.txt-sort --mt ../tmp/lsq-2bit_from_jing.txt-sort 
+python ../ldn-quantization/tools.py --keyword update,raw --old pretrained/fcos/R_50_1x-FPN_SyncBN-FCOS-SyncBN_no_lp_pretrained_${bit}bit/model_final.pth --new pretrained/fcos/R_50_1x-FPN_SyncBN-FCOS-SyncBN_no_lp_pretrained_${bit}bit/import_jing.pth --mf ../tmp/fcos-r50-from.txt-sort --mt ../tmp/fcos-r50-to.txt-sort
 
 python ../ldn-quantization/tools.py --keyword reshape --old pretrained/fcos/R_50_1x-FPN_SyncBN-FCOS-SyncBN_no_lp_pretrained_${bit}bit/import_jing.pth --new pretrained/fcos/R_50_1x-FPN_SyncBN-FCOS-SyncBN_no_lp_pretrained_${bit}bit/reshape.pth --mf weights/det-resnet18/reshape.txt 
 
+cd -
