496f95a9f0a46bda7f504a6f97d645d6e6c9c610
titanx8 <titanx8>
2020-11-10 07:30:07 +0800

    Fix bugs

--- retina-r34-convert.sh
+++ retina-r34-convert.sh
@@ -1,7 +1,8 @@
 
 cd ../detectron2
 
-bit=2
+bit=4
+
 python ../ldn-quantization/tools.py --keyword update,raw --old pretrained/retinanet/retinanet_R_34_1x_Full_SyncBN_dorefa_clip_${bit}bit_no_lp_pretrained/model_final.pth --new pretrained/retinanet/retinanet_R_34_1x_Full_SyncBN_dorefa_clip_${bit}bit_no_lp_pretrained/error-fpn.pth --mf ../tmp/retina-r34_from.txt-sort --mt ../tmp/retina-r34_to.txt-sort 
 
 python ../ldn-quantization/tools.py --keyword reshape --mf weights/det-resnet18/reshape.txt --old pretrained/retinanet/retinanet_R_34_1x_Full_SyncBN_dorefa_clip_${bit}bit_no_lp_pretrained/error-fpn.pth --new pretrained/retinanet/retinanet_R_34_1x_Full_SyncBN_dorefa_clip_${bit}bit_no_lp_pretrained/error-reshape.pth
